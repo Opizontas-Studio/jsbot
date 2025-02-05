aec57fa6e5c08ae97be96081a74f539e5c8f5315
durvia <phenakso@gmail.com>
2025-02-05 15:39:54 +0000

    adjust: 修改重启频率

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     out_file: 'logs/out.log',
     time: true,
     // 每3小时重启一次
-    cron_restart: '0 */3 * * *',
+    cron_restart: '0 */6 * * *',
     // 优雅关闭
     kill_timeout: 10000,
     wait_ready: true,
